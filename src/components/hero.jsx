68f19568e9ea3a39a0b4f7ff23b18b05b23368f3
Bhavneek Singh <bhavneek2002@gmail.com>
2025-01-27 07:14:02 +0900

    infinite text 1

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,11 +1,25 @@
+import { motion } from "motion/react"
+
 const Hero = () => {
   return (
-    <div className="border-solic border-[5px] border-orange-500 w-screen h-screen bg-white flex justify-center items-center">
-        <div className="font-playfair text-[76px] flex flex-row gap-[20px] justify-start flex-nowrap overflow-hidden">
+    <div className="border-solic border-[5px] border-orange-500 w-screen h-screen bg-white flex justify-center items-center overflow-hidden whitespace-nowrap">
+        <motion.div 
+        animate={{
+          translateX: ['0px', '-565px'], //!!!!!!!!!!!! fixed pixel size
+          transition: {
+            duration: 3,
+            ease: 'linear',
+            repeat: Infinity,
+          }
+        }}
+      
+        // !!!!!!!!!!!!!!!!!! fixed font size
+        className="w-[calc(100vw-20px)] font-playfair text-[76px] flex flex-row gap-[20px] justify-start flex-nowrap ">
           <p className="min-w-[12ch] m-0 p-0">Bhavneek Singh</p>
           <p className="min-w-[12ch] m-0 p-0">Bhavneek Singh</p>
           <p className="min-w-[12ch] m-0 p-0">Bhavneek Singh</p>
-        </div>
+          <p className="min-w-[12ch] m-0 p-0">Bhavneek Singh</p>
+        </motion.div>
     </div>
   )
 }
