ab6dde405f78a9f66746a1f917add3408ff02f3e
blazingbhavneek <bhavneek2002@gmail.com>
2025-01-29 16:28:47 +0900

    velocity text done

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,39 +1,11 @@
-import { motion } from "framer-motion";
-import { useEffect, useRef, useState } from "react";
+import VelocityText from "./velocityText";
 
 const Hero = () => {
-  const textSize=32;
-  const gapBetween=20;
-  const pRef = useRef(null);
-  const [textWidth, setTextWidth] = useState(0);
-
-  useEffect(() => {
-    if (pRef.current) {
-      setTextWidth(pRef.current.clientWidth);
-    }
-  }, []);
 
   return (
-    <div className="border-solic border-[5px] border-orange-500 w-screen h-screen bg-white flex justify-center items-center overflow-hidden whitespace-nowrap">
-      <motion.div
-        animate={{
-          translateX: [0, -textWidth-gapBetween], 
-          transition: {
-            duration: 3,
-            ease: 'linear',
-            repeat: Infinity,
-          }
-        }}
-        className={`w-[calc(100vw-20px)] font-playfair text-[${textSize}px] flex flex-row gap-[${gapBetween}px] justify-start flex-nowrap`}
-      >
-        <p ref={pRef} className="min-w-[12ch] m-0 p-0">Bhavneek Singh</p>
-        <p className="min-w-[12ch] m-0 p-0">Bhavneek Singh</p>
-        <p className="min-w-[12ch] m-0 p-0">Bhavneek Singh</p>
-        <p className="min-w-[12ch] m-0 p-0">Bhavneek Singh</p>
-        <p className="min-w-[12ch] m-0 p-0">Bhavneek Singh</p>
-        <p className="min-w-[12ch] m-0 p-0">Bhavneek Singh</p>
-        <p className="min-w-[12ch] m-0 p-0">Bhavneek Singh</p>
-      </motion.div>
+    <div className="border-solid border-[5px] border-orange-500 w-screen h-screen bg-white flex flex-col justify-center items-center overflow-hidden whitespace-nowrap">
+      <VelocityText text="Bhavneek" textSize={36} gapBetween={20} baseVelocity={100}></VelocityText>
+      <VelocityText text="Singh" textSize={36} gapBetween={20} baseVelocity={-100}></VelocityText>  
     </div>
   );
 };
---
+++ src/components/velocityText.jsx
@@ -0,0 +1,75 @@
+/* eslint-disable react/prop-types */
+import { motion, 
+    useMotionValue,
+    useTransform,
+    useScroll,
+    useVelocity,
+    useSpring,
+    useAnimationFrame,
+    useMotionValueEvent
+  } from "framer-motion";
+  import { wrap } from "@motionone/utils";
+  import {useState, useRef, useEffect} from "react";
+  
+  const VelocityText = (props) => {
+  
+    const pRef = useRef(null);
+    const [textWidth, setTextWidth] = useState(0);
+    const movementWidth=textWidth+props.gapBetween;
+    useEffect(() => {
+      if (pRef.current) {
+        setTextWidth(pRef.current.offsetWidth);
+      }
+    }, []);
+  
+    const baseX = useMotionValue(0);
+    const { scrollY } = useScroll();
+    const scrollVelocity = useVelocity(scrollY);
+    const smoothVelocity = useSpring(scrollVelocity, {
+      damping: 50,
+      stiffness: 400
+    });
+    const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 10], {
+      clamp: false
+    });
+
+    const [scrollDirection, setScrollDirection] = useState(1)
+
+    useMotionValueEvent(scrollY, "change", (current) => {
+    const diff = current - scrollY.getPrevious()
+    setScrollDirection(diff > 0 ? 1 : -1)
+    })
+  
+    useAnimationFrame((t, delta) => {
+      let moveBy = scrollDirection*props.baseVelocity * (delta / 1000);
+      moveBy+=scrollDirection*velocityFactor.get()*moveBy
+      baseX.set(baseX.get() + moveBy);
+    });
+  
+    const x = useTransform(baseX, (v) => `${wrap(-2*movementWidth, -movementWidth, v)}px`);
+    return (
+    <div className="w-screen bg-white flex justify-center items-center overflow-hidden whitespace-nowrap">
+        <motion.div style={{x}}
+          className={`w-[calc(100vw-20px)] font-playfair text-[${props.textSize}px] flex flex-row gap-[${props.gapBetween}px] justify-start flex-nowrap`}
+        >
+          <p ref={pRef} className="m-0 p-0">{props.text}</p>
+          <p className="m-0 p-0">{props.text}</p>
+          <p className="m-0 p-0">{props.text}</p>
+          <p className="m-0 p-0">{props.text}</p>
+          <p className="m-0 p-0">{props.text}</p>
+          <p className="m-0 p-0">{props.text}</p>
+          <p className="m-0 p-0">{props.text}</p>
+          <p className="m-0 p-0">{props.text}</p>
+          <p className="m-0 p-0">{props.text}</p>
+          <p className="m-0 p-0">{props.text}</p>
+          <p className="m-0 p-0">{props.text}</p>
+          <p className="m-0 p-0">{props.text}</p>
+          <p className="m-0 p-0">{props.text}</p>
+          <p className="m-0 p-0">{props.text}</p>
+          <p className="m-0 p-0">{props.text}</p>
+        </motion.div>
+      </div>
+    );
+  }
+
+  export default VelocityText;
