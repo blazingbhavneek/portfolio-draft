dd372c88d35ba233b0a39a90666d5c7e650e5320
blazingbhavneek <bhavneek2002@gmail.com>
2025-01-30 15:59:56 +0900

    minor improvements

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -5,10 +5,10 @@ import {motion} from "framer-motion";
 
 const Hero = () => {
   return (
-    <div className="border-solid border-[5px] border-orange-500 w-screen h-screen bg-white flex flex-col justify-center items-center overflow-hidden whitespace-nowrap relative">
+    <div className="w-screen h-screen bg-white flex flex-col justify-center items-center overflow-hidden whitespace-nowrap relative">
       <div className="z-20 bg-transparent">
-        <VelocityText text="Bhavneek" textSize={36} gapBetween={20} baseVelocity={100} />
-        <VelocityText text="Singh" textSize={36} gapBetween={20} baseVelocity={-100} />
+        <VelocityText text="Bhavneek" textProps="font-playfair text-[36px] text-gray-900" gapBetween={20} baseVelocity={100} />
+        <VelocityText text="Singh" textProps="font-playfair text-[36px] text-gray-900" gapBetween={20} baseVelocity={-100} />
       </div>
 
       <motion.div 
@@ -18,10 +18,10 @@ const Hero = () => {
         // whileHover={{ scale: 1.2, zIndex: 100}}
         // whileTap={{ scale: 1.1, zIndex: 100}}
       >
-        <img src={Mine} alt="Logo" className="z-10 absolute inset-0 w-full h-full object-cover" />
+        <img src={Mine} alt="Logo" className="blur-[2px] z-10 absolute inset-0 w-full h-full object-cover" />
         <motion.img 
           initial={{ scale: 0 }}
-          animate={{ scale: 1}}
+          animate={{ scale: 1.05}}
           whileHover={{ scale: 1.2, zIndex: 100}}
           whileTap={{ scale: 1.1, zIndex: 100}}
           src={MineBgRem} alt="Logo" className="z-30 absolute inset-0 w-full h-full object-cover mix-blend-normal" />
